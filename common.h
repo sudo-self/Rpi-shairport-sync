7285b1ee0364a9d3cda1824cc7f4799faa42b1b0
Pieter De Gendt <pieter.degendt@gmail.com>
2019-11-29 14:57:35 +0100

    Jack output add soxr resampling

--- common.h
+++ common.h
@@ -270,6 +270,9 @@ typedef struct {
 #ifdef CONFIG_JACK
   char *jack_client_name;
   char *jack_autoconnect_pattern;
+#ifdef CONFIG_SOXR
+  int jack_soxr_resample_recipe;
+#endif
 #endif
 
 } shairport_cfg;
