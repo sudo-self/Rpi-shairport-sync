efec3cc7e5201e149bcbb2da9bb129526cfab422
Mike Brady <mikebradydublin@icloud.com>
2020-06-09 09:36:12 +0100

    Make the list of remote to local timing values a power of 2 -- necessary later for allowing headroom for calculations.

--- player.h
+++ player.h
@@ -24,7 +24,8 @@
 #include "alac.h"
 #include "audio.h"
 
-#define time_ping_history 128 // at 1 per three seconds, approximately six minutes of records
+#define time_ping_history_power_of_two 7
+#define time_ping_history (1 << time_ping_history_power_of_two) // 2^7 is 128. At 1 per three seconds, approximately six minutes of records
 
 typedef struct time_ping_record {
   uint64_t local_to_remote_difference;
